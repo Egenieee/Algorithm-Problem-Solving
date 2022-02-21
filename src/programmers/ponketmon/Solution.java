eb745d0460c5af4649b56c302d599bf7d9de832e
JinHee Lee <hhgg0925@naver.com>
2022-02-21 12:27:01 +0900

    solve no.1845

---
+++ src/programmers/ponketmon/Solution.java
@@ -0,0 +1,30 @@
+package programmers.ponketmon;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int result = solution.solution(new int[]{3,1,2,3,4,5});
+        System.out.println(result);
+    }
+    public int solution(int[] nums) {
+        int limitCount = nums.length / 2;
+
+        Set<Integer> ponketmons = new HashSet<>();
+
+        // Set에 넣기
+        for ( int ponketmon : nums ) {
+            ponketmons.add(ponketmon);
+        }
+
+        // Set의 길이가 limitCount와 같거나 작으면 Set의 길이 반환, 크면 limitCount반환
+        if(ponketmons.size() <= limitCount) {
+            return ponketmons.size();
+        } else {
+            return limitCount;
+        }
+    }
+
+}
