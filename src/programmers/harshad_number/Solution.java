b5ceedc7767f9b17aebbd2e3c02df4ebc12a65fb
Egenieee <hhgg0925@naver.com>
2022-03-05 19:50:44 +0900

    solve no.12947

---
+++ src/programmers/harshad_number/Solution.java
@@ -0,0 +1,25 @@
+package programmers.harshad_number;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(12));
+    }
+    public boolean solution(int x) {
+        boolean answer = false;
+        int sum = 0;
+
+        String number = String.valueOf(x);
+        char[] numbers = number.toCharArray();
+
+        for (char num : numbers) {
+            sum += (num - '0');
+        }
+
+        if (x % sum == 0) {
+            answer = true;
+        }
+
+        return answer;
+    }
+}
