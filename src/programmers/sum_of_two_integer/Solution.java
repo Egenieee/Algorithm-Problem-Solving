0facef16507ef6546dd9ae9b85da895c563949e6
Egenieee <hhgg0925@naver.com>
2022-03-21 20:51:16 +0900

    solve no.12912

---
+++ src/programmers/sum_of_two_integer/Solution.java
@@ -0,0 +1,21 @@
+package programmers.sum_of_two_integer;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(3, 5));
+    }
+    public long solution(int a, int b) {
+        long answer = 0;
+        int count = 0;
+        int start = Math.min(a, b);
+
+        while (count <= Math.abs(a - b)) {
+            answer += start;
+            start++;
+            count++;
+        }
+
+        return answer;
+    }
+}
