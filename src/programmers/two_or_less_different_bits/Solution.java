d7dc18c60bd8a2a09de44f320b136c238ac54e95
JinHee Lee <hhgg0925@naver.com>
2022-03-24 10:15:26 +0900

    no.77885 테스트 케이스 2개 시간 초과 - 하나 씩 비교

---
+++ src/programmers/two_or_less_different_bits/Solution.java
@@ -0,0 +1,89 @@
+package programmers.two_or_less_different_bits;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 7	000...0111
+// 11   000...1011
+
+// 8	000...1000	4
+// 9	000...1001	3
+// 10	000...1010	3
+// 11	000...1011	2 --> 제일 작음
+// 12   000...1100  3
+// 13   000...1101  2
+// 14   000...1110  2
+// 15   000...1111  1
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(new long[]{1001,337,0,1,333,673,343,221,898,997,121,1015,665,779,891,421,222,256,512,128,100}));
+    }
+    public List<Long> solution(long[] numbers) {
+        List<Long> answer = new ArrayList<>();
+
+        for (long number : numbers) {
+            long fx = f(number);
+            answer.add(fx);
+        }
+
+        return answer;
+    }
+    private long f (long number) {
+        // number보다 2개 이하로 비트가 다른 수 중에 제일 작은 수
+
+        long min = Long.MAX_VALUE;
+        int diff = 0;
+
+        for (int i = 1 ; diff != 2 ; i++) {
+            long counterpart = number + i;
+            diff = getDiff(number, counterpart);
+            if (min > counterpart && diff <= 2) {
+                min = counterpart;
+            }
+        }
+
+        return min;
+    }
+
+    private int getDiff(long number, long counterpart) {
+        String numberBitsBeforeSetting = Long.toBinaryString(number);
+        String counterpartBitsBeforeSetting = Long.toBinaryString(counterpart);
+
+        int max = Math.max(numberBitsBeforeSetting.length(), counterpartBitsBeforeSetting.length());
+        int diff = Math.abs(numberBitsBeforeSetting.length() - counterpartBitsBeforeSetting.length());
+
+        String numberBits = getNumberBits(numberBitsBeforeSetting, max, diff);
+        String counterpartBits = getCounterpartBits(counterpartBitsBeforeSetting, max, diff);
+
+        // 다른 비트 개수 세기
+        int count = 0;
+
+        for (int i = 0 ; i < numberBits.length() ; i++) {
+            if (numberBits.charAt(i) != counterpartBits.charAt(i)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private String getNumberBits(String numberBitsBeforeSetting, int max, int diff) {
+        if (numberBitsBeforeSetting.length() == max) {
+            return numberBitsBeforeSetting;
+        }
+        String zeroes = "0".repeat(diff);
+
+        return zeroes + numberBitsBeforeSetting;
+    }
+
+    private String getCounterpartBits(String counterpartBitsBeforeSetting, int max, int diff) {
+        if (counterpartBitsBeforeSetting.length() == max) {
+            return counterpartBitsBeforeSetting;
+        }
+        String zeroes = "0".repeat(diff);
+
+        return zeroes + counterpartBitsBeforeSetting;
+    }
+}
