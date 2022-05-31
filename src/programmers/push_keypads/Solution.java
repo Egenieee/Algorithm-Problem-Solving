f32c54d32e447642f34a561adab005cb2a517a27
JinHee Lee <hhgg0925@naver.com>
2022-05-31 15:15:09 +0900

    refactor no.67256 - Hand class 이동

--- src/programmers/push_keypads/Solution.java
+++ src/programmers/push_keypads/Solution.java
@@ -77,27 +77,5 @@ public class Solution {
 
         return coordinates;
     }
-
-    public class Hand {
-        int x;
-        int y;
-
-        public Hand (int initX, int initY) {
-            this.x = initX;
-            this.y = initY;
-        }
-
-        public void changeHand(List<Integer> coordinates) {
-            this.x = coordinates.get(0);
-            this.y = coordinates.get(1);
-        }
-
-        public int getDistanceFromKey(List<Integer> coordinates) {
-            int targetNumberX = coordinates.get(0);
-            int targetNumberY = coordinates.get(1);
-
-            return Math.abs(x - targetNumberX) + Math.abs(y - targetNumberY);
-        }
-    }
 }
 
