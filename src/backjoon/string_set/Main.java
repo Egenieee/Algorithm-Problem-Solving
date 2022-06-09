ba8cd4c36ddb374995b24f8d873beb56b629da15
JinHee Lee <hhgg0925@naver.com>
2022-06-09 11:15:08 +0900

    solve no.14425

---
+++ src/backjoon/string_set/Main.java
@@ -0,0 +1,35 @@
+package backjoon.string_set;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int count = 0;
+
+        Set<String> S = new HashSet<>();
+
+        for (int i = 0 ; i < N ; i++) {
+            S.add(br.readLine());
+        }
+
+        for (int i = 0 ; i < M ; i++) {
+            if (S.contains(br.readLine())) {
+                count++;
+            }
+        }
+
+        bw.write(String.valueOf(count));
+        bw.flush();
+        bw.close();
+    }
+}
