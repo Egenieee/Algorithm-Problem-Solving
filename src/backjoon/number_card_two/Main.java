133015a888898b4f8316919da4639b66f996bc7d
JinHee Lee <hhgg0925@naver.com>
2022-06-09 14:02:08 +0900

    solve no.10816

---
+++ src/backjoon/number_card_two/Main.java
@@ -0,0 +1,41 @@
+package backjoon.number_card_two;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        Map<Integer, Integer> cardMap = new HashMap<>();
+
+        for (int i = 0 ; i < N ; i++) {
+            int card = Integer.parseInt(st.nextToken());
+            cardMap.put(card, cardMap.getOrDefault(card, 0) + 1);
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0 ; i < M ; i++) {
+            int search = Integer.parseInt(st.nextToken());
+            if (cardMap.containsKey(search)) {
+                sb.append(cardMap.get(search)).append(" ");
+            } else {
+                sb.append(0).append(" ");
+            }
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
