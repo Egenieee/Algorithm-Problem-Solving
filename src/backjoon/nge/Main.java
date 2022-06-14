77a745d6e881fea7b706f5427e67ebbed290e3c6
JinHee Lee <hhgg0925@naver.com>
2022-06-14 12:38:48 +0900

    solving no.17298 - list로 풀어서 시간초과 ^^

--- src/backjoon/nge/Main.java
+++ src/backjoon/nge/Main.java
@@ -1,6 +1,8 @@
 package backjoon.nge;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -13,39 +15,67 @@ public class Main {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        Stack<Integer> stack = new Stack<>();
+        //Stack<Integer> stack = new Stack<>();
+        List<Integer> list = new ArrayList<>();
 
         for (int i = 0 ; i < size ; i++) {
-            stack.push(Integer.parseInt(st.nextToken()));
+            list.add(Integer.parseInt(st.nextToken()));
         }
 
-        String[] result = new String[stack.size()];
+//        for (int i = 0 ; i < size ; i++) {
+//            stack.push(Integer.parseInt(st.nextToken()));
+//        }
 
-        int current = stack.pop();
-        int previous = current;
-        int max = current;
-        int nge = -1;
+        int nge;
 
-        int resultIdx = stack.size();
+        String[] result = new String[list.size()];
 
-        result[resultIdx--] = String.valueOf(nge);
-
-        while (!stack.empty()) {
-            current = stack.pop();
-            if (current < previous) {
-                nge = previous;
-            } else if (current < max) {
-                nge = max;
-            } else if (current > max) {
-                nge = -1;
-                //max = current;
-            }
-            result[resultIdx--] = String.valueOf(nge);
-            previous = current;
+        for (int i = 0 ; i < size ; i++) {
+            nge = getNGE(i, list);
+            result[i] = String.valueOf(nge);
         }
+
+//        int next = 0;
+//        int current = stack.pop();
+//        int previous = current;
+//        int max = current;
+//        int nge = -1;
+//
+//        int resultIdx = stack.size();
+//
+//        result[resultIdx--] = String.valueOf(nge);
+//
+//        while (!stack.empty()) {
+//            current = stack.pop();
+//            if (current < previous) {
+//                nge = previous;
+//            } else if (current > max) {
+//                nge = -1;
+//                max = current;
+//            } else if (current < max && current > nge ) {
+//                max = current;
+//            }
+////            else if (current < nge) {
+////                nge = current;
+////            }
+//            result[resultIdx--] = String.valueOf(nge);
+//            previous = current;
+//        }
 
         bw.write(String.join(" ", result));
         bw.flush();
         bw.close();
+    }
+
+    private static int getNGE(int i, List<Integer> list) {
+        int number = list.get(i);
+
+        for (int idx = i ; idx < list.size() ; idx++) {
+            if (list.get(idx) > number) {
+                return list.get(idx);
+            }
+        }
+
+        return -1;
     }
 }
