04d3c541ae8da3930a4ad02744669cb121910700
JinHee Lee <hhgg0925@naver.com>
2022-06-14 13:56:31 +0900

    solve no.17298 - 결국 답 봄 얏호 ^^ 나중에 다시 풀어보자

--- src/backjoon/nge/Main.java
+++ src/backjoon/nge/Main.java
@@ -1,8 +1,6 @@
 package backjoon.nge;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -10,72 +8,43 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
         int size = Integer.parseInt(br.readLine());
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        //Stack<Integer> stack = new Stack<>();
-        List<Integer> list = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+
+        int[] numbers = new int[size];
 
         for (int i = 0 ; i < size ; i++) {
-            list.add(Integer.parseInt(st.nextToken()));
+            numbers[i] = Integer.parseInt(st.nextToken());
         }
 
-//        for (int i = 0 ; i < size ; i++) {
-//            stack.push(Integer.parseInt(st.nextToken()));
-//        }
+        // stack 안에 있는 값은 바뀌어야 할 인덱스
+        // 현재 보고 있는 값과 스택의 맨 위에 있는 값을 비교해서 현재 값이 더 크면, 오큰수가 되기 때문에
+        // 스택에서 팝하고 그 인덱스에 현재 보고 있는 값을 할당한다.
+        for (int currentIdx = 0 ; currentIdx < size ; currentIdx++) {
+            while (!stack.isEmpty() && numbers[stack.peek()] < numbers[currentIdx]) {
+                numbers[stack.pop()] = numbers[currentIdx];
+            }
 
-        int nge;
-
-        String[] result = new String[list.size()];
-
-        for (int i = 0 ; i < size ; i++) {
-            nge = getNGE(i, list);
-            result[i] = String.valueOf(nge);
+            stack.push(currentIdx);
         }
 
-//        int next = 0;
-//        int current = stack.pop();
-//        int previous = current;
-//        int max = current;
-//        int nge = -1;
-//
-//        int resultIdx = stack.size();
-//
-//        result[resultIdx--] = String.valueOf(nge);
-//
-//        while (!stack.empty()) {
-//            current = stack.pop();
-//            if (current < previous) {
-//                nge = previous;
-//            } else if (current > max) {
-//                nge = -1;
-//                max = current;
-//            } else if (current < max && current > nge ) {
-//                max = current;
-//            }
-////            else if (current < nge) {
-////                nge = current;
-////            }
-//            result[resultIdx--] = String.valueOf(nge);
-//            previous = current;
-//        }
+        // stack 에 남아있는 인덱스는 오큰수를 찾지 못했다는 뜻이 되므로 다 -1로 바꿔준다.
 
-        bw.write(String.join(" ", result));
+        while (!stack.isEmpty()) {
+            numbers[stack.pop()] = -1;
+        }
+
+        for (int number : numbers) {
+            sb.append(number).append(" ");
+        }
+
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
-    }
-
-    private static int getNGE(int i, List<Integer> list) {
-        int number = list.get(i);
-
-        for (int idx = i ; idx < list.size() ; idx++) {
-            if (list.get(idx) > number) {
-                return list.get(idx);
-            }
-        }
-
-        return -1;
     }
 }
