dcf97c9c08487f58513f2a2968cebaf53ba35304
JinHee Lee <hhgg0925@naver.com>
2022-06-22 09:21:30 +0900

    solve no.1037

---
+++ src/backjoon/divisor/Main.java
@@ -0,0 +1,35 @@
+package backjoon.divisor;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int countOfDivisor = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+
+        int minDivisor = Integer.MAX_VALUE;
+        int maxDivisor = Integer.MIN_VALUE;
+
+        for (int i = 0 ; i < countOfDivisor ; i++) {
+            int divisor = Integer.parseInt(st.nextToken());
+
+            if (minDivisor >= divisor) {
+                minDivisor = divisor;
+            }
+
+            if (maxDivisor <= divisor) {
+                maxDivisor = divisor;
+            }
+        }
+
+        bw.write(String.valueOf(maxDivisor * minDivisor));
+        bw.flush();
+        bw.close();
+    }
+}
