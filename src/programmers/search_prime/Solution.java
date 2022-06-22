1cbf9ad61ef1b9a803bea4516f49e20838ba1c8a
Egenieee <hhgg0925@naver.com>
2022-06-22 20:24:42 +0900

    solve no.42839 - 순열 문제 - DFS를 이용해서 풀 수 있었다. 개념이 헷갈리므로 꼭 다시 풀어보기.

---
+++ src/programmers/search_prime/Solution.java
@@ -0,0 +1,90 @@
+package programmers.search_prime;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    Set<Integer> numberPermutation = new HashSet<>();
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution("12"));
+    }
+
+    public int solution(String numbers) {
+        int countOfPrime = 0;
+
+        String[] inputNumbers = getInputNumbers(numbers);
+        String[] output = new String[numbers.length()];
+        boolean[] visited = new boolean[numbers.length()];
+
+        // 순열 구하기
+        for (int digit = 1; digit <= numbers.length(); digit++) {
+            getPermutation(inputNumbers, output, visited, 0, digit);
+        }
+
+        // 소수인지 확인하기
+        for (int number : numberPermutation) {
+            if (isPrime(number)) {
+                countOfPrime++;
+            }
+        }
+
+        return countOfPrime;
+    }
+
+    private String[] getInputNumbers(String numbers) {
+        String[] inputNumbers = new String[numbers.length()];
+
+        for (int i = 0; i < numbers.length(); i++) {
+            inputNumbers[i] = String.valueOf(numbers.charAt(i));
+        }
+
+        return inputNumbers;
+    }
+
+    // curNumLen -> depth, 깊이만큼 순열을 뽑는다.
+    // digit -> 뽑아야 하는 숫자 개수
+    // 현재 깊이와 digit이 같다면 순열 만들기 끝 -> 저장하자.
+    private void getPermutation(String[] numbers, String[] output, boolean[] visited, int curNumLen, int digit) {
+        if (curNumLen == digit) {
+            savePermutation(output, digit);
+            return;
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                output[curNumLen] = numbers[i];
+                getPermutation(numbers, output, visited, curNumLen + 1, digit);
+                visited[i] = false;
+            }
+        }
+    }
+
+    private void savePermutation(String[] arr, int r) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < r; i++) {
+            sb.append(arr[i]);
+        }
+        numberPermutation.add(Integer.parseInt(sb.toString()));
+    }
+
+    private boolean isPrime(int number) {
+        int divisor = 2;
+
+        if (number <= 1) {
+            return false;
+        }
+
+        while (divisor < number) {
+            if (number % divisor == 0) {
+                return false;
+            }
+            divisor++;
+        }
+
+        return true;
+    }
+}
