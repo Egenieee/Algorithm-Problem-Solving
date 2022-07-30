33a3faeb2a9cd077905fd877c90eb33c98552090
Egenieee <hhgg0925@naver.com>
2022-07-31 00:43:21 +0900

    solve ss_sw_algorithm no.6

--- src/etc/ss_sw_algorithm/no_6/Solution.java
+++ src/etc/ss_sw_algorithm/no_6/Solution.java
@@ -29,8 +29,50 @@ public class Solution {
             st = new StringTokenizer(sc.nextLine());
 
             for (int j = 0; j < commandCount; j++) {
-
+                String command = st.nextToken();
+                if (command.equals("I")) {
+                    insert(linkedList, st);
+                } else if (command.equals("D")) {
+                    delete(linkedList, st);
+                } else {
+                    append(linkedList, st);
+                }
             }
+
+            System.out.print("#" + test_case + " ");
+
+            for (int k = 0; k < 10; k++) {
+                System.out.print(linkedList.get(k) + " ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    private static void insert(List<String> linkedList, StringTokenizer st) {
+        int idx = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            linkedList.add(idx, st.nextToken());
+            idx++;
+        }
+    }
+
+    private static void delete(List<String> linkedList, StringTokenizer st) {
+        int idx = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            linkedList.remove(idx);
+        }
+    }
+
+    private static void append(List<String> linkedList, StringTokenizer st) {
+        int n = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            linkedList.add(st.nextToken());
         }
     }
 }
