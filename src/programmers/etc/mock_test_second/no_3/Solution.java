1be3ca7578f49782900529e2151cdc88f9fbd1fe
Egenieee <hhgg0925@naver.com>
2022-08-09 20:56:35 +0900

    solving mock test no_3 - 시간초과 5개

--- src/programmers/etc/mock_test_second/no_3/Solution.java
+++ src/programmers/etc/mock_test_second/no_3/Solution.java
@@ -6,6 +6,58 @@ import java.util.List;
 import java.util.PriorityQueue;
 
 public class Solution {
+    private int searchMinimumComebackDistance(List<Edge>[] adjacentInfo, int n, int start, int destination) {
+        // 시작 지역부터 각 지역에 도달하는 최소 시간 저장할 배열 선언
+        int[] distance = new int[n + 1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+
+        PriorityQueue<Edge> queue = new PriorityQueue<>();
+
+        // 시작 지역을 엣지로 만들어서 큐에 푸쉬한다.
+        queue.add(new Edge(start, 0));
+
+        // 시작점에서 시작점으로 이동하는 시간은 0이므로 초기화
+        distance[start] = 0;
+
+        // 큐가 빌때까지 반복한다.
+        while (!queue.isEmpty()) {
+            // 큐에 있는 간선을 하나 뽑아와서
+            Edge edge = queue.poll();
+
+            // 현재 보고 있는 간선이 잇고 있는 지역 번호와 가중치를 받아온다.
+            int currentRegion = edge.number;
+            int currentDistance = edge.distance;
+
+            if (distance[currentRegion] < currentDistance) {
+                continue;
+            }
+
+            if (currentRegion == destination) {
+                break;
+            }
+
+            // 받아온 간선과 연결되어 있는 인접한 간선들을 쭉 보면서 거쳐서 온 경로의 가중치가 더 작다면 갱신해준다.
+            for (int i = 0; i < adjacentInfo[currentRegion].size(); i++) {
+                // 현재 보고 있는 간선과 연결된 지역을 받아온다.
+                int newRegion = adjacentInfo[currentRegion].get(i).number;
+                // 현재 보고 있는 간선을 거쳐온 경로의 가중치를 새로 만들어준다.
+                int newDistance = adjacentInfo[currentRegion].get(i).distance + currentDistance;
+
+                // 새로 만든 가중치가 원래의 가중치보다 작으면 새롭게 만든 가중치로 바꿔준다.
+                if (distance[newRegion] > newDistance) {
+                    distance[newRegion] = newDistance;
+                    queue.add(new Edge(newRegion, newDistance));
+                }
+            }
+        }
+
+        if (distance[destination] == Integer.MAX_VALUE) {
+            return -1;
+        }
+
+        return distance[destination];
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] answer = solution.solution(5, new int[][]{{1, 2}, {2, 3}}, new int[]{2, 3}, 1);
@@ -36,58 +88,6 @@ public class Solution {
         }
 
         return result;
-    }
-
-    private int searchMinimumComebackDistance(List<Edge>[] adjacentInfo, int n, int start, int destination) {
-        // 시작 지역부터 각 지역에 도달하는 최소 시간 저장할 배열 선언
-        int[] distance = new int[n + 1];
-        Arrays.fill(distance, Integer.MAX_VALUE);
-
-        boolean[] visited = new boolean[n + 1];
-
-        PriorityQueue<Edge> queue = new PriorityQueue<>();
-
-        // 1번 마을을 엣지로 만들어서 큐에 푸쉬한다.
-        queue.add(new Edge(start, 0));
-
-        // 시작점에서 시작점으로 이동하는 시간은 0이므로 초기화
-        distance[start] = 0;
-
-        // 큐가 빌때까지 반복한다.
-        while (!queue.isEmpty()) {
-            // 큐에 있는 간선을 하나 뽑아와서
-            Edge edge = queue.poll();
-
-            // 현재 보고 있는 간선이 잇고 있는 지역 번호와 가중치를 받아온다.
-            int currentRegion = edge.number;
-            int currentDistance = edge.distance;
-
-            if (!visited[currentRegion]) {
-                visited[currentRegion] = true;
-                if (currentRegion == destination) {
-                    break;
-                }
-                // 받아온 간선과 연결되어 있는 인접한 간선들을 쭉 보면서 거쳐서 온 경로의 가중치가 더 작다면 갱신해준다.
-                for (int i = 0; i < adjacentInfo[currentRegion].size(); i++) {
-                    // 현재 보고 있는 간선과 연결된 지역을 받아온다.
-                    int newRegion = adjacentInfo[currentRegion].get(i).number;
-                    // 현재 보고 있는 간선을 거쳐온 경로의 가중치를 새로 만들어준다.
-                    int newDistance = adjacentInfo[currentRegion].get(i).distance + currentDistance;
-
-                    // 새로 만든 가중치가 원래의 가중치보다 작으면 새롭게 만든 가중치로 바꿔준다.
-                    if (distance[newRegion] > newDistance) {
-                        distance[newRegion] = newDistance;
-                        queue.add(new Edge(newRegion, newDistance));
-                    }
-                }
-            }
-        }
-
-        if (distance[destination] == Integer.MAX_VALUE) {
-            return -1;
-        }
-
-        return distance[destination];
     }
 
     static class Edge implements Comparable<Edge> {
