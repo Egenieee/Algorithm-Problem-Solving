94f4ac18613eca4e5898b4dd7d7ff1819d23628f
Egenieee <hhgg0925@naver.com>
2023-01-05 23:27:51 +0900

    refactor code

--- src/programmers/level_1/nearest_same_letter/Solution.java
+++ src/programmers/level_1/nearest_same_letter/Solution.java
@@ -15,17 +15,22 @@ public class Solution {
         List<Integer> answer = new ArrayList<>();
         Map<Character, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < s.length(); i++) {
-            if (!map.containsKey(s.charAt(i))) {
-                answer.add(-1);
-                map.put(s.charAt(i), i);
-            } else {
-                int diff = i - map.get(s.charAt(i));
-                answer.add(diff);
-                map.put(s.charAt(i), i);
-            }
+        for (int index = 0; index < s.length(); index++) {
+            saveBeforeSameLetterPosition(map, s, index, answer);
         }
 
         return answer;
+    }
+
+    private void saveBeforeSameLetterPosition(Map<Character, Integer> map, String s, int index, List<Integer> answer) {
+        if (!map.containsKey(s.charAt(index))) {
+            answer.add(-1);
+            map.put(s.charAt(index), index);
+            return;
+        }
+
+        int beforeSameLetterIndex = index - map.get(s.charAt(index));
+        answer.add(beforeSameLetterIndex);
+        map.put(s.charAt(index), index);
     }
 }
