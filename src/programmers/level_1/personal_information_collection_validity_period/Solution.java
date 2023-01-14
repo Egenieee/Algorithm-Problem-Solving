aaa4f071740f15067bf8a2657bb50e18395812d0
Egenieee <hhgg0925@naver.com>
2023-01-14 21:31:50 +0900

    solve no.150370

---
+++ src/programmers/level_1/personal_information_collection_validity_period/Solution.java
@@ -0,0 +1,72 @@
+package programmers.level_1.personal_information_collection_validity_period;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution("2022.05.19",
+                new String[]{"A 6", "B 12", "C 3"},
+                new String[]{"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"}));
+    }
+
+    public List<Integer> solution(String today, String[] terms, String[] privacies) {
+        List<Integer> informationToDestroy = new ArrayList<>();
+
+        LocalDate todayDate = getDateFormat(today);
+        Map<String, Integer> validityPeriod = getValidityPeriodTable(terms);
+
+        for (int i = 0; i < privacies.length; i++) {
+            if (isExpiredInformation(privacies[i], validityPeriod, todayDate)) {
+                informationToDestroy.add(i + 1);
+            }
+        }
+
+        return informationToDestroy;
+    }
+
+    private boolean isExpiredInformation(String eachPrivacy, Map<String, Integer> validityPeriod, LocalDate todayDate) {
+        String[] privacy = eachPrivacy.split(" ");
+
+        LocalDate date = getDateFormat(privacy[0]);
+        int monthToAdd = validityPeriod.get(privacy[1]);
+
+        LocalDate dateNew = date.plusMonths(monthToAdd).minusDays(1);
+
+        return dateNew.isBefore(todayDate);
+    }
+
+    private LocalDate getDateFormat(String today) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy.MM.dd");
+        LocalDate localDate = null;
+
+        try {
+            Date todayDate = simpleDateFormat.parse(today);
+
+            // Date -> LocalDate 변환
+            localDate = todayDate.toInstant()   // Date -> Instant
+                    .atZone(ZoneId.systemDefault())  // Instant -> ZonedDateTime
+                    .toLocalDate(); // ZonedDateTime -> LocalDate
+        } catch (ParseException exception) {
+            exception.printStackTrace();
+        }
+
+        return localDate;
+    }
+
+    private Map<String, Integer> getValidityPeriodTable(String[] terms) {
+        Map<String, Integer> validityPeriod = new HashMap<>();
+
+        for (String term : terms) {
+            String[] validity = term.split(" ");
+
+            validityPeriod.put(validity[0], Integer.valueOf(validity[1]));
+        }
+
+        return validityPeriod;
+    }
+}
