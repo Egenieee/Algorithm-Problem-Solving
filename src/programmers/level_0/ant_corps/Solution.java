f283a9a00e895ececfe2a900648a27fc04b6b3f5
Egenieee <hhgg0925@naver.com>
2023-01-15 16:11:52 +0900

    refactor: 코드 수정
    - 어차피 가장 큰 수 부터 몫과 나머지만 받아오면 되니 반복문과 조건문이 필요없다.

--- src/programmers/level_0/ant_corps/Solution.java
+++ src/programmers/level_0/ant_corps/Solution.java
@@ -11,18 +11,13 @@ public class Solution {
 
         int[] ants = new int[]{1, 3, 5};
 
-        while (hp != 0) {
-            if (hp >= ants[2]) {
-                totalAntNumber += hp / ants[2];
-                hp %= ants[2];
-            } else if (hp >= ants[1]) {
-                totalAntNumber += hp / ants[1];
-                hp %= ants[1];
-            } else {
-                totalAntNumber += hp / ants[0];
-                hp %= ants[0];
-            }
-        }
+        totalAntNumber += hp / ants[2];
+        hp %= ants[2];
+
+        totalAntNumber += hp / ants[1];
+        hp %= ants[1];
+
+        totalAntNumber += hp / ants[0];
 
         return totalAntNumber;
     }
