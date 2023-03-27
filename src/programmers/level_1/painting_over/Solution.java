7eb42b5fcbdc3edc07d85b1157d9dcc0c2797bd7
Egenieee <hhgg0925@naver.com>
2023-03-27 22:00:39 +0900

    solve no.161989

--- src/programmers/level_1/painting_over/Solution.java
+++ src/programmers/level_1/painting_over/Solution.java
@@ -1,62 +1,22 @@
 package programmers.level_1.painting_over;
 
-import java.util.Arrays;
-
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution(8, 2, new int[]{2, 6}));
+        System.out.println(solution.solution(4, 1, new int[]{1, 2, 3, 4}));
     }
 
     public int solution(int n, int m, int[] section) {
         int paintingCount = 0;
-
-        boolean[] painted = getPaintWall(n, section);
+        int maxPaintedIndex = 0;
 
         for (int part : section) {
-            if (isRepainted(painted, m, part)) {
-                paint(painted, m, part);
+            if (part > maxPaintedIndex) {
+                maxPaintedIndex = part - 1 + m;
                 paintingCount++;
             }
         }
 
         return paintingCount;
-    }
-
-    private void paint(boolean[] painted, int m, int part) {
-        for (int i = 0; i < m; i++) {
-            if (isValidRange(painted, part, i)) {
-                painted[part - 1 + i] = true;
-            }
-        }
-    }
-
-    private boolean isRepainted(boolean[] painted, int m, int part) {
-        for (int i = 0; i < m; i++) {
-            if (isValidRange(painted, part, i)) { // 안칠해진 곳이 있으면 다시 칠해야한다고 반환
-                if (!painted[part - 1 + i]) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
-
-    private boolean isValidRange(boolean[] painted, int part, int i) {
-        return painted.length > part - 1 + i;
-    }
-
-    private boolean[] getPaintWall(int n, int[] section) {
-        boolean[] painted = new boolean[n];
-
-        Arrays.fill(painted, true);
-
-        // 다시 칠해야 하는 구역은 false로 초기화
-        for (int part : section) {
-            painted[part - 1] = false;
-        }
-
-        return painted;
     }
 }
