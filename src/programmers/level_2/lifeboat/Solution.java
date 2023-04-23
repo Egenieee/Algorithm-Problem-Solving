cd000d0f931b17328223d7bdb0d4e45a88a8e66e
Egenieee <hhgg0925@naver.com>
2023-04-23 23:07:54 +0900

    solve no.42885

--- src/programmers/level_2/lifeboat/Solution.java
+++ src/programmers/level_2/lifeboat/Solution.java
@@ -1,8 +1,6 @@
 package programmers.level_2.lifeboat;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Solution {
     public static void main(String[] args) {
@@ -13,45 +11,22 @@ public class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
 
-        List<Integer> sortedPeople = getSortedPeople(people);
+        Arrays.sort(people);
 
-        while (sortedPeople.size() > 0) {
-            int savedPeopleCount = getSavedPeopleCount(sortedPeople, limit);
+        int minWeightIndex = 0;
+        int maxWeightIndex = people.length - 1;
 
-            if (savedPeopleCount > 0) {
+        while (minWeightIndex <= maxWeightIndex) {
+            if (people[minWeightIndex] + people[maxWeightIndex] <= limit) {
+                minWeightIndex++;
+                maxWeightIndex--;
+                answer++;
+            } else {
+                maxWeightIndex--;
                 answer++;
             }
         }
 
         return answer;
-    }
-
-    private List<Integer> getSortedPeople(int[] people) {
-        Arrays.sort(people);
-        List<Integer> sortedPeople = new ArrayList<>();
-
-        for (int i = people.length - 1; i >= 0; i--) {
-            sortedPeople.add(people[i]);
-        }
-
-        return sortedPeople;
-    }
-
-    private int getSavedPeopleCount(List<Integer> people, int limit) {
-        int currentPerson = people.get(0);
-
-        int savedPeopleCount = 1;
-
-        for (int i = 1; i < people.size(); i++) {
-            if (currentPerson + people.get(i) <= limit) {
-                savedPeopleCount++;
-                people.remove(i);
-                people.remove(0);
-                return savedPeopleCount;
-            }
-        }
-
-        people.remove(0);
-        return savedPeopleCount;
     }
 }
