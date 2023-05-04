a8a77e09dc5bc93031281ed300ae5a23215cb9d1
Egenieee <hhgg0925@naver.com>
2023-05-05 00:17:39 +0900

    solve no.2439

---
+++ src/baekjoon/bronze/bronze_4/stars/Main.java
@@ -0,0 +1,25 @@
+package baekjoon.bronze.bronze_4.stars;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int starCount = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= starCount; i++) {
+            for (int j = i; j < starCount; j++) {
+                bw.write(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
